d1e90b7fd8975a70c44846b2018b75e75b49b6ae
timbaginski <54997067+timbaginski@users.noreply.github.com>
2021-10-01 22:15:24 -0400

    Add files via upload

--- nitSimulator.py
+++ nitSimulator.py
@@ -15,24 +15,40 @@ def percent_at_income(df: pd.DataFrame, income: float) -> float:
             (income - incomes[l]) / (incomes[h] - incomes[l])) / 100
 
 
+# Given an income and a threshold, calculate amount somebody with that income saves from new taxes
+def taxes_saved(threshold: float, income: int, tax_rates: pd.DataFrame) -> float:
+    temp = min(threshold, income)
+    taxes = 0.0
+    for index, value in tax_rates["BRACKET"].items():
+        if temp <= 0:
+            break
+        taxes += min(temp, value) * (tax_rates["RATE"][index] / 100)
+        temp -= value
+
+    return taxes
+
+
 # returns the total cost of a NIT program given a threshold and percent
 # In addition to all workers, includes non-working non-retired adults
-def payment_cost(threshold: float, percent: float, df: pd.DataFrame) -> float:
+def payment_cost(threshold: float, percent: float, df: pd.DataFrame, tax_rates: pd.DataFrame) -> float:
     res, prev_percent = 0.0, 0.0
     for i in range(0, int(threshold), 100):
         curr = percent_at_income(df, i)
-        res += (curr - prev_percent) * NUM_WORKERS * ((threshold - i) * percent)
+        tax_savings = taxes_saved(threshold, i, tax_rates) * (curr - prev_percent) * NUM_WORKERS
+        res += (curr - prev_percent) * NUM_WORKERS * ((threshold - i) * percent) + tax_savings
         prev_percent = curr
 
-    return (res + ((NUM_ADULTS_US - NUM_WORKERS- NUM_RETIRED_ADULTS) *
+    return (res + ((NUM_ADULTS_US - NUM_WORKERS - NUM_RETIRED_ADULTS) *
                    (threshold * percent)))
 
 
 def main():
-    df = pd.read_csv('data.csv')
+    income_percentiles = pd.read_csv('data.csv')
+    tax_rates = pd.read_csv("tax_rates.csv")
     threshold = float(input("What threshold do you want the NIT to apply to?"))
     percent = float((input("What percent of the threshold minus income should the NIT give?")))
-    cost = round(payment_cost(threshold, percent / 100, df), 2)
+    cost = round(payment_cost(threshold, percent / 100, income_percentiles, tax_rates), 2)
+
     print("A NIT guaranteeing " + str(percent) + "% of a threshold of $" +
           str(threshold) + " would cost: $" + f'{cost:,}')
 
